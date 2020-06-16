b0b6f12599eb93da0e8f87a22d4aeeb25733d259
Taylor Downs <downs.taylor@gmail.com>
2020-06-16 16:49:53 -0600

    shell for visits

---
+++ add-visits.js
@@ -0,0 +1,32 @@
+alterState(state => {
+  function clean(obj) {
+    for (var propName in obj) {
+      if (obj[propName] === '-' || obj[propName] === null) {
+        delete obj[propName];
+      }
+    }
+    return obj;
+  }
+
+  state.data = {
+    visits: state.data.visits.map(c => {
+      delete c.CreatedById;
+      delete c.LastModifiedById;
+      c.Name = 'MFI Clinic';
+      return clean(c);
+    }),
+  };
+
+  return state;
+});
+
+bulk(
+  'Account',
+  'upsert',
+  {
+    extIdField: 'CAST_Location_ID__c',
+    failOnError: true,
+    allowNoOp: true,
+  },
+  dataValue('visits')
+);

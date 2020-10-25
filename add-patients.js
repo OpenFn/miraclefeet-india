230dfaf8802ec1b31212a1c64a72fb300759ae75
Aleksa Krolls <aleksagk@gmail.com>
2020-10-25 23:49:08 +0200

    aleksa@openfn.org's change on OpenFn.org

--- add-patients.js
+++ add-patients.js
@@ -22,8 +22,9 @@ alterState(state => {
       p.Gender__c = p.gciclubfoot__Gender__c;
       delete p.gciclubfoot__Gender__c;
 
-      p['Account.CAST_Location_ID__c'] = p.CAST_Locaion_ID__c;
-      delete p.CAST_Locaion_ID__c;
+      //p['Account.CAST_Location_ID__c'] = p.CAST_Locaion_ID__c;
+      //delete p.CAST_Locaion_ID__c;
+      p['Account.CAST_Location_ID__c'] = p.CAST_Location_ID__c;
       
       //delete from upload; we can't update this in SF unless setting enabled?
       delete p.CreatedById;

61faf8da057f5ee053bc34f516d088ddc3d49069
Aleksa Krolls <aleksagk@gmail.com>
2021-03-02 20:19:06 +0200

    aleksa@openfn.org's change on OpenFn.org

--- add-patients.js
+++ add-patients.js
@@ -32,9 +32,10 @@ alterState(state => {
       p.Relapse_Type_Right_India__c = p.Relapse_Type_Right_Foot__c
         ? p.Relapse_Type_Right_Foot__c
         : '';
-        
-      p.Date_of_Tenotomy__c = p.Date_of_Tenotomy__c
-        ? new Date(p.Date_of_Tenotomy__c).toISOString() : ''; 
+      
+      //TODO: Convert Date_of_Tetonomy__c when state is in format '24-10-2019'
+      /*p.Date_of_Tenotomy__c = p.Date_of_Tenotomy__c
+        ? new Date(p.Date_of_Tenotomy__c).toISOString() : ''; */
 
       //SF field = India field;
       //p.gciclubfoot_CAST_Patient_ID__c = p.CAST_Patient_ID__c; //confirm mapping

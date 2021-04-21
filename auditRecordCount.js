126fabc88d7d1a3803bdcee9c6b9f7b824aa3572
Aleksa Krolls <aleksagk@gmail.com>
2021-04-21 07:49:47 +0300

    aleksa@openfn.org's change on OpenFn.org

--- auditRecordCount.js
+++ auditRecordCount.js
@@ -1,1 +1,10 @@
-// Your job goes here.
+alterState(state => {
+  const { Patient, Visit, Clinic, VisitDeleted, LookupTable } = state.data;
+
+  console.log(`Total count of clinic sets  ${Clinic.length}.`);
+  console.log(`Total count of patient sets  ${Patient.length}.`);
+  console.log(`Total count of visit sets  ${Visit.length}.`);
+  console.log(`Total count of deleted visit sets  ${VisitDeleted.length}.`);
+
+  return state;
+});
